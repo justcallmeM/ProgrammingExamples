4000dc0982f16d373e92fcbaec0321a05a79687d
Mindaugas Pikelis <36482664+justcallmeM@users.noreply.github.com>
2022-09-20 20:55:13 +0300

    abstract comments

--- Abstract/Models/Guitar.cs
+++ Abstract/Models/Guitar.cs
@@ -10,6 +10,9 @@
             base.Weight = weight;
         }
 
+        //we must use override keyword before the method
+        //is declared as abstract in the child class
+
         public override void MakeNoise()
         {
             Console.WriteLine("Strum or pick the guitar strings");
--- Abstract/Models/Trumpet.cs
+++ Abstract/Models/Trumpet.cs
@@ -4,6 +4,9 @@
 
     public class Trumpet : Instrument
     {
+        //we must use override keyword before the method
+        //is declared as abstract in the child class
+
         public override void MakeNoise()
         {
             Console.WriteLine("Blow into the horn");
