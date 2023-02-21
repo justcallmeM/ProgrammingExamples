355bf16899337cd357568fd9b58ea43433f6eb3f
Mindaugas Pikelis <36482664+justcallmeM@users.noreply.github.com>
2023-02-21 23:22:09 +0200

    delegate example

---
+++ Delegates/Calculator.cs
@@ -0,0 +1,41 @@
+﻿namespace Delegates
+{
+    public class Calculator
+    {
+        public delegate double Operation(double x, double y);
+
+        public static double Initiate()
+        {
+            Console.WriteLine("Enter first number and press enter");
+            int x = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter second number and press enter");
+            int y = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(
+                @"On the keypad press one of the symbols for:
+                    multiplication: *
+                    division: /
+                    addition: +
+                    subtraction: -");
+
+            return Console.ReadKey().Key switch
+            {
+                ConsoleKey.Multiply => Calculate(x, y, (x, y) => x * y),
+                ConsoleKey.Divide   => Calculate(x, y, (x, y) => x / y),
+                ConsoleKey.Add      => Calculate(x, y, (x, y) => x + y),
+                ConsoleKey.Subtract => Calculate(x, y, (x, y) => x - y),
+                _ => throw new ArgumentException(),
+            };
+        }
+
+        public static double Calculate(double x, double y, Operation operation)
+        {
+            //validate x
+            //validate y
+
+            //log something
+
+            return operation(x, y);
+        }
+    }
+}
---
+++ Delegates/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Delegates
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculator.Initiate();
+        }
+
+        //A delegate is a type that represents references to methods with a particular parameters list nd return type.
+        //A delegate is a type that safely encapsulates a method.
+
+        //Check multicast delegate
+    }
+}
